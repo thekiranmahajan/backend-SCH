84731a9cec8e24fb7e2df10d52f2f4a8806f7919
Kiran Mahajan <91893931+thekiranmahajan@users.noreply.github.com>
2023-12-07 13:45:23 +0530

    ejs setup

--- expressJS/script.js
+++ expressJS/script.js
@@ -1,8 +1,22 @@
-const express = requre("express");
+const express = require("express");
 const app = express();
+
+
+app.set("view engine", "ejs")
 
 app.get("/", function (req, res) {
   res.send("hello Express");
 });
 
+app.get("/contact", function (req, res) {
+  res.send("this is a contact page");
+});
+
+app.get("/profile", function (req, res) {
+  res.render("Profile Page");
+});
+
+app.get("/profile/:username", function (req, res) {
+  res.send(`this is your username: ${req.params.username}`);
+});
 app.listen(3000);
